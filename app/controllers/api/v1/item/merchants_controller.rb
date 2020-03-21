8705c634b77e057df9679e914b37df86ea703caa
Ryan Allen <rcallen89@gmail.com>
2020-03-21 15:27:26 -0600

    Moved Search Controllers

--- app/controllers/api/v1/item/merchants_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::V1::Item::MerchantsController < ApplicationController
-
-  def show
-    render json: MerchantSerializer.new(Item.find(params[:item_id]).merchant)
-  end
-  
-end
-
--- app/controllers/api/v1/merchant/items_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::V1::Merchant::ItemsController < ApplicationController
-
-  def index
-    render json: ItemSerializer.new(Merchant.find(params[:merchant_id]).items)
-  end
-
-
-end
