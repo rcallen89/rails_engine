58cb6b92386e5bd94dab63aa1a43308e712b6138
Ryan Allen <rcallen89@gmail.com>
2020-03-22 13:59:40 -0600

    Create Item Method Helpers w/ Spec

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,18 @@ class Item < ApplicationRecord
                         :unit_price
 
   belongs_to :merchant
-  has_many :invoice_items
+  has_many :invoice_items, dependent: :destroy
   has_many :invoices, through: :invoice_items
+
+  def self.query_string(search_params)
+    search_params.keys.map {|att| "#{att} ILIKE '%#{search_params[att]}%'"}.join(' AND ')
+  end
+
+  def self.single_search(search_params)
+    where(query_string(search_params)).first
+  end
+
+  def self.multi_search(search_params)
+    where(query_string(search_params))
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,4 +12,45 @@ RSpec.describe Item, type: :model do
     it { should have_many :invoice_items }
     it { should have_many(:invoices).through(:invoice_items) }
   end
+
+  describe 'methods' do
+    it 'can return a string for sql fragment' do
+      search_params = {name: "Item Name", description: "Description"}
+
+      expect(Item.query_string(search_params)).to eq("name ILIKE '%Item Name%' AND description ILIKE '%Description%'")
+    end
+    it "can find a single item based on a partial attribute" do
+      item = create(:item, name: "teST ITEM", description: "THIS is a Test ITEM")
+      # extra items to test what comes back
+      create_list(:item, 3)
+
+      search_params = {name: "tem"}
+
+      expect(Item.single_search(search_params)).to eq(item)
+    end
+
+    it "can find a single item based on a multiple partial attributes" do
+      item = create(:item, name: "Red Pen", description: "Red")
+      item_2 = create(:item, name: "Blue Pen", description: "Blue")
+      
+      # extra items to test what comes back
+      create_list(:item, 3)
+
+      search_params = {name: "pen", description: "blue"}
+
+      expect(Item.single_search(search_params)).to eq(item_2)
+    end
+
+    it "can find a many items based on attributes" do
+      item_1 = create(:item, name: "Red Pen", description: "Red")
+      item_2 = create(:item, name: "Blue Pen", description: "Blue")
+      
+      # extra items to test what comes back
+      create_list(:item, 3)
+
+      search_params = {name: "pen"}
+
+      expect(Item.multi_search(search_params)).to eq([item_1, item_2])
+    end
+  end
 end
