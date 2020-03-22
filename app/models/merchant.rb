abcf906725c29d965eaa8f2ab1a9e0a52e2954d1
Ryan Allen <rcallen89@gmail.com>
2020-03-22 13:59:56 -0600

    Create Merchant Method Helpers /w Spec

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,5 +1,17 @@
 class Merchant < ApplicationRecord
   validates_presence_of :name
 
-  has_many :items
+  has_many :items, dependent: :destroy
+
+  def self.query_string(search_params)
+    search_params.keys.map {|att| "#{att} ILIKE '%#{search_params[att]}%'"}.join(' AND ')
+  end
+
+  def self.single_search(search_params)
+    where(query_string(search_params)).first
+  end
+
+  def self.multi_search(search_params)
+    where(query_string(search_params))
+  end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -8,4 +8,33 @@ RSpec.describe Merchant, type: :model do
   describe 'relationships' do
     it { should have_many :items }
   end
+
+  describe 'methods' do
+    it 'can return a string for sql fragment' do
+      search_params = {name: "Maybe Name"}
+
+      expect(Merchant.query_string(search_params)).to eq("name ILIKE '%Maybe Name%'")
+    end
+    it "can find a single merchant based on a partial attribute" do
+      merchant = create(:merchant, name: "teST Merchant")
+      # extra merchant to test what comes back
+      create_list(:merchant, 3)
+
+      search_params = {name: "MERCHANT"}
+
+      expect(Merchant.single_search(search_params)).to eq(merchant)
+    end
+
+    it "can find a many merchants based on attributes" do
+      merchant_1 = create(:merchant, name: "Red MerchANT")
+      merchant_2 = create(:merchant, name: "Blue Merchant")
+      
+      # extra items to test what comes back
+      create_list(:merchant, 3)
+
+      search_params = {name: "merchant"}
+
+      expect(Merchant.multi_search(search_params)).to eq([merchant_1, merchant_2])
+    end
+  end
 end
