1d03dbce932b6abcec089aba91adc3a32f59f039
Ryan Allen <rcallen89@gmail.com>
2020-03-22 12:33:34 -0600

    Move routes to fix sequence

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,23 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show, :create, :update, :destroy] do
-        scope module: :merchant do
-          resources :items, only: [:index]
-        end
+      get '/items/find', to: 'items/search#show'
+      get '/items/find_all', to: 'items/search#index'
+
+      resources :merchants, only: [:index, :show, :create, :update, :destroy]
+      resources :items, only: [:index, :show, :create, :update, :destroy]
+      namespace :items do
+        # resources :merchant, only: [:show]
+        get '/:id/merchant', to: 'merchant#show'
+        # get '/find', to: "search#show"
       end
-      resources :items, only: [:index, :show, :create, :update, :destroy] do
-        scope module: :item do
-          resource :merchant, only: [:show]
-        end
+
+      namespace :merchants do
+        # resources :items, only: [:index]
+        get '/:id/items', to: 'items#index'
       end
+
+
     end
   end
 end
