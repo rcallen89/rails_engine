d84b28ccded044ffad718195fcea59e4f02d5562
Ryan Allen <rcallen89@gmail.com>
2020-03-23 08:15:13 -0600

    Modify Transaction Factory

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :transaction do
     invoice
     credit_card_number { 1234123412341234 }
-    credit_card_expiration { 1 }
+    credit_card_expiration_date { 1 }
     result { "success" }
   end
 end
